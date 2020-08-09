22c70d8fe674687ae87d7191718e17332f828a4e
DrorMaman <drormaman@gmail.com>
2020-08-09 15:46:29 +0300

    organized script.js

--- script.js
+++ script.js
@@ -3,85 +3,111 @@ const productInput = document.querySelector('#productInput');
 const priceInput = document.querySelector('#priceInput');
 const button = document.querySelector('#addProductBtn');
 
-const API_URL = 'http://localhost:3000'
-axios.defaults.baseURL = API_URL;
+axios.defaults.baseURL = 'http://localhost:3000';
 
+//get all products from api
 function getAllProducts() {
-    return axios.get('/products').then(res => res.data)
+    return axios.get('/products')
+        .then(res => res.data)
+        .catch(error => error);
 }
+
+//initialize the list with the products from the api
 async function showList() {
-    productsList.innerHTML = "";
-    products = await getAllProducts();
-    products.map((product) => {
-        console.log(product);
-        addProductFromObj(product);
-    })
+    try {
+        productsList.innerHTML = "";
+        products = await getAllProducts();
+        products.map((product) => {
+            addProductFromObj(product);
+        })
+    } catch (error) {
+        alert("there was a problem while getting the products");
+        console.log(error.message);
+    }
+
 }
 
 function addProductFromObj(productObj) {
-    const productName = productObj.productName;
-    const productPrice = productObj.price;
-    const productId = productObj.id;
     productInput.value = '';
-
+    // creating all li elements
     const productLi = document.createElement('li');
     const productTextSpan = document.createElement('span');
     const productPriceSpan = document.createElement('span');
     const deleteButton = document.createElement('button');
     const editButton = document.createElement('button');
-
+    //product name span
     productLi.appendChild(productTextSpan);
-    productTextSpan.textContent = productName;
+    productTextSpan.textContent = productObj.productName;;
     productTextSpan.classList.add('textSpan');
-
+    //product price span
     productLi.appendChild(productPriceSpan);
-    productPriceSpan.textContent = productPrice + ' ₪';
+    productPriceSpan.textContent = productObj.price; + ' ₪';
     productPriceSpan.classList.add('priceSpan');
-
+    //delete button
     productLi.appendChild(deleteButton);
     deleteButton.textContent = 'Delete';
     deleteButton.classList.add('deleteBtn');
     deleteButton.addEventListener('click', () => {
-        deleteProduct(productId);
+        deleteProduct(productObj.id);
         productsList.removeChild(productLi);
     });
-
+    //edit button
     productLi.appendChild(editButton);
     editButton.textContent = 'Edit';
     editButton.classList.add('editBtn');
     editButton.addEventListener('click', () => {
         editProduct(productObj);
-        // showList();
     });
-
+    //adding li to ul
     productsList.appendChild(productLi);
     productLi.classList.add('productLi');
     productInput.focus();
 }
+
 //POST
 async function postProduct() {
-    const newProductObj = await axios.post('/products', {
-        productName: productInput.value,
-        price: priceInput.value
-    }).then(res => res.data);
-    addProductFromObj(newProductObj);
+    try {
+        const newProductObj = await axios.post('/products', {
+                productName: productInput.value,
+                price: priceInput.value
+            }).then(res => res.data)
+            .catch(error => error);
+        addProductFromObj(newProductObj);
+    } catch (error) {
+        alert("there was a problem while adding the product");
+        console.log(error.message);
+    }
+
 }
 
 //DELETE
 function deleteProduct(id) {
-    axios.delete(`/products/${id}`);
+    axios.delete(`/products/${id}`).catch(error => {
+        console.log(error);
+        alert('there was a problem deleting the product');
+    });
 }
+
 //UPDATE 
 async function editProduct(productObj) {
-    const editedProduct = {
-        id: productObj.id,
-        productName: prompt('Edit product name:'),
-        price: prompt('Edit product price:')
+    try {
+        const editedProduct = {
+            id: productObj.id,
+            productName: prompt('Edit product name:'),
+            price: prompt('Edit product price:')
+        }
+        await axios.put(`/products/${productObj.id}`, editedProduct);
+        showList();
+    } catch (error) {
+        alert("there was a problem while updating the product");
+        console.log(error.message);
     }
-    await axios.put(`/products/${productObj.id}`, editedProduct);
-    showList();
+
 }
 
-
 window.addEventListener('load', showList);
-button.addEventListener('click', postProduct);
+button.addEventListener('click', (event) => {
+    if (productInput.value !== "") {
+        postProduct();
+    }
+});
